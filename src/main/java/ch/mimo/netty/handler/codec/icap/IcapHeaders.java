4aabcebb3a873a996ee17594f23b0192c080fa58
Michael Mimo Moratti <mimo@mimo.ch>
2011-04-03 08:44:12 +0200

    removed unused classes

--- src/main/java/ch/mimo/netty/handler/codec/icap/IcapHeaders.java
+++ src/main/java/ch/mimo/netty/handler/codec/icap/IcapHeaders.java
@@ -13,6 +13,8 @@
  *******************************************************************************/
 package ch.mimo.netty.handler.codec.icap;
 
+import java.io.Serializable;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -370,6 +372,25 @@ public final class IcapHeaders {
         @Override
         public String toString() {
             return key + "=" + value;
+        }
+    }
+    
+    private static final class CaseIgnoringComparator implements Comparator<String>, Serializable {
+
+        private static final long serialVersionUID = 4582133183775373862L;
+
+        static final CaseIgnoringComparator INSTANCE = new CaseIgnoringComparator();
+
+        private CaseIgnoringComparator() {
+            super();
+        }
+
+        public int compare(String o1, String o2) {
+            return o1.compareToIgnoreCase(o2);
+        }
+
+        private Object readResolve() {
+            return INSTANCE;
         }
     }
 }
