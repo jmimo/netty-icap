de663d43190d86ad164d6f9571dae290b7cfb74f
Michael Mimo Moratti <mimo@mimo.ch>
2011-04-26 16:20:36 +0200

    lowered complexity and added todo

--- src/main/java/ch/mimo/netty/handler/codec/icap/ReadIcapHeaderState.java
+++ src/main/java/ch/mimo/netty/handler/codec/icap/ReadIcapHeaderState.java
@@ -37,22 +37,14 @@ public class ReadIcapHeaderState extends State<Object> {
 		for(String[] header : headerList) {
 			icapMessageDecoder.message.addHeader(header[0],header[1]);
 		}
-		if(!icapMessageDecoder.isDecodingResponse()) {
-			// validate mandatory icap headers
-			if(!icapMessageDecoder.message.containsHeader(IcapHeaders.Names.HOST)) {
-				throw new IcapDecodingError("Mandatory ICAP message header [Host] is missing");
-			}
-		}
-		if(!icapMessageDecoder.message.containsHeader(IcapHeaders.Names.ENCAPSULATED)) {
-			throw new IcapDecodingError("Mandatory ICAP message header [Encapsulated] is missing");
-		}
+		validateMandatoryMessageHeaders(icapMessageDecoder.message,icapMessageDecoder.isDecodingResponse());
 		Encapsulated encapsulated = new Encapsulated(icapMessageDecoder.message.getHeader(IcapHeaders.Names.ENCAPSULATED));
 		icapMessageDecoder.message.setEncapsulatedHeader(encapsulated);
-		
+		// TODO find less complex and more generic solution. This statement assumes that there is a res hds always after the reqhdr.
+		// this by the way is correct when assuming the other end is adhering 100% to the rfc!
 		if(!encapsulated.containsEntry(IcapMessageElementEnum.REQHDR) & !encapsulated.containsEntry(IcapMessageElementEnum.RESHDR)) {
 			return StateReturnValue.createRelevantResult(icapMessageDecoder.message);
-		}
-		if(icapMessageDecoder.message.getMethod().equals(IcapMethod.OPTIONS)) {
+		} else if(icapMessageDecoder.message.getMethod().equals(IcapMethod.OPTIONS)) {
 			return StateReturnValue.createRelevantResult(icapMessageDecoder.message);
 		}
 		return StateReturnValue.createIrrelevantResult();
@@ -73,19 +65,27 @@ public class ReadIcapHeaderState extends State<Object> {
 			if(entry != null) {
 				if(entry.equals(IcapMessageElementEnum.REQHDR)) {
 					return StateEnum.READ_HTTP_REQUEST_INITIAL_AND_HEADERS;
-				}
-				if(entry.equals(IcapMessageElementEnum.RESHDR)) {
+				} else if(entry.equals(IcapMessageElementEnum.RESHDR)) {
 					return StateEnum.READ_HTTP_RESPONSE_INITIAL_AND_HEADERS;
-				}
-				if(entry.equals(IcapMessageElementEnum.REQBODY)) {
+				} else if(entry.equals(IcapMessageElementEnum.REQBODY)) {
 					return StateEnum.READ_CHUNK_SIZE_STATE;
-				}
-				if(entry.equals(IcapMessageElementEnum.RESBODY)) {
+				} else if(entry.equals(IcapMessageElementEnum.RESBODY)) {
 					return StateEnum.READ_CHUNK_SIZE_STATE;
 				}
 			}
 		}
 		return null;
+	}
+	
+	private void validateMandatoryMessageHeaders(IcapMessage message, boolean isDecodingResponse) {
+		if(!isDecodingResponse) {
+			if(!message.containsHeader(IcapHeaders.Names.HOST)) {
+				throw new IcapDecodingError("Mandatory ICAP message header [Host] is missing");
+			}
+		}
+		if(!message.containsHeader(IcapHeaders.Names.ENCAPSULATED)) {
+			throw new IcapDecodingError("Mandatory ICAP message header [Encapsulated] is missing");
+		}
 	}
 
 }
