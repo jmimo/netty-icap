b1b26ce4af55eeb35b16de5db57e0bd7b02c8624
Michael Mimo Moratti <mimo@mimo.ch>
2011-05-06 08:09:27 +0200

    removed unused constructors

--- src/main/java/ch/mimo/netty/handler/codec/icap/DecodingException.java
+++ src/main/java/ch/mimo/netty/handler/codec/icap/DecodingException.java
@@ -10,19 +10,6 @@ public class DecodingException extends Exception {
 	
 	private static final long serialVersionUID = 1318955320625997060L;
 
-	
-	public DecodingException() {
-		super();
-	}
-
-	public DecodingException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public DecodingException(String message) {
-		super(message);
-	}
-
 	public DecodingException(Throwable cause) {
 		super(cause);
 	}
